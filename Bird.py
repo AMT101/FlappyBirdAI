8b0445fadbd923235522b5f8b829c4a158ed8d21
AMT101 <amitanshupattanayak99@gmail.com>
2020-12-29 21:37:40 +0530

    drawing the animated bird on window

--- Bird.py
+++ Bird.py
@@ -1,15 +1,66 @@
 import pygame
 import os
+import math
 
 
 class Bird:
     """
     This class represents the flappy bird
     """
-    IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", f"bird{x}.png"))) for x in range(1, 4)]
+    IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", f"bird{x}.png"))) for x in range(1, 5)]
+    MAX_ROTATION = 25
+    ROTATION_VELOCITY = 20
+    ANIMATION_TIME = 5
 
-    def __init__(self):
+    def __init__(self, x, y):
         """
-        Initialize the bird object
+        Initializes the Bird object
+        :param x: x-coordinate of the bird's current position
+        :param y: y-coordinate of the bird's current position
+        :return: None
         """
+        self.x = x
+        self.y = y
         self.img = self.IMGS[0]
+        self.vel = 0
+        self.tilt = 0
+        self.ticks = 0
+        self.height = self.y
+        self.img_count = 0
+
+    def draw(self, win):
+        """
+        Displays bird object on the window
+        :param win: pygame window
+        :return: None
+        """
+
+        # For animating the bird flapping
+        if self.ANIMATION_TIME*4 <= self.img_count:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+        else:
+            ind = math.floor(self.img_count / self.ANIMATION_TIME)
+            self.img = self.IMGS[ind]
+
+        # For the time when the bird is free falling
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2 - 1
+
+        self.img_count += 1
+
+        # rotating the image
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        """
+        get the mask for the current image of the bird
+        :return: mask of the bird
+        """
+        return pygame.mask.from_surface(self.img)
+
+
+
--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-
+import os
 
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
@@ -9,12 +9,37 @@ DRAW_LINES = False
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 
+bg_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "bg.png")).convert_alpha(), (600, 900))
 
-if __name__ == '__main__':
+
+def draw_window(win, bird):
+    win.blit(bg_img, (0, 0))
+    bird.draw(win)
+    pygame.display.update()
+
+
+def main():
+    """
+    main thread to run first
+    :return: None
+    """
     # objects to be used once the display loads
     from Bird import Bird
     from Pipe import Pipe
     from Base import Base
 
-    bird = Bird()
-    print(bird.img)
+    bird = Bird(250, 450)
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw_window(WIN, bird)
+    pygame.quit()
+    quit()
+
+
+if __name__ == '__main__':
+    main()
