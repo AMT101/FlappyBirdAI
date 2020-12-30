89f4771278bc181b6905c570e762d548e1601b19
AMT101 <amitanshupattanayak99@gmail.com>
2020-12-30 19:58:08 +0530

    added movement mechanic to pipe object

--- Pipe.py
+++ Pipe.py
@@ -1,5 +1,7 @@
 import pygame
 import os
+import random
+import game
 
 
 class Pipe:
@@ -7,11 +9,84 @@ class Pipe:
     This class represents the Pipe
     """
     IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")).convert_alpha())
+    GAP = 250
+    VEL = 5
 
-    def __init__(self, x):
+    def __init__(self, x, win_height, win_width):
         """
-        Initialize the pipe object
+        Initializes the Pipe object
+        :param x: initial x-coordinate of the pipe's position
+        :param win_height: Height of the window created
         """
         self.img = self.IMG
         self.x = x
-        
+        self.win_height = win_height
+        self.win_width = win_width
+
+        self.top = 0
+        self.bottom = 0
+
+        self.TOP_PIPE = pygame.transform.flip(self.IMG, False, True)
+        self.BOTTOM_PIPE = self.IMG
+
+        self.passed = False
+        self.out_screen = False
+
+        self.set_height()
+
+    def set_height(self):
+        """
+        Sets the height of the pipe from the top of the screen
+        :return: None
+        """
+        self.height = random.randrange(50, self.win_height-50)
+        self.top = self.height - self.TOP_PIPE.get_height()
+        self.bottom = self.height + self.GAP
+
+
+    def move(self):
+        """
+        Moves the pipe leftwards with constant velocity
+        :return: None
+        """
+        self.x -= self.VEL
+        # if pipe passes the screen
+        if self.x < -20 and not self.out_screen:
+            self.out_screen = True
+            self.reset()
+
+    def reset(self):
+        """
+        resets the initial state of the pipe to be used again (Object Pooling)
+        :return: None
+        """
+        self.passed = False
+        game.add_pipe(self)
+
+    def set_reset_pipe(self, x):
+        """
+        Function to be used by the main module, to reset the state of the pipe
+        :param x: x-coordinate of the pipe
+        :return: None
+        """
+        self.x = x
+        self.out_screen = False
+        self.set_height()
+
+
+    def draw(self, win):
+        """
+        Draw the pipes
+        :param win: pygame window
+        :return: None
+        """
+        # draw top
+        win.blit(self.TOP_PIPE, (self.x, self.top))
+        # draw bottom
+        win.blit(self.BOTTOM_PIPE, (self.x, self.bottom))
+
+
+
+
+
+
--- game.py
+++ game.py
@@ -11,10 +11,25 @@ pygame.display.set_caption("Flappy Bird")
 
 bg_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "bg.png")).convert_alpha(), (600, 900))
 
+PIPE_QUEUE = []
 
-def draw_window(win, bird):
+
+def add_pipe(pipe):
+    """
+    Adds the pipe that has gone out of the window to PIPE_QUEUE to be used again
+    :param pipe: pipe object that has gone out of the window
+    :return: None
+    """
+    global PIPE_QUEUE
+    PIPE_QUEUE.append(pipe)
+    print(PIPE_QUEUE)
+
+
+def draw_window(win, bird, pipe):
     win.blit(bg_img, (0, 0))
     bird.draw(win)
+    pipe.draw(win)
+
     pygame.display.update()
 
 
@@ -29,6 +44,7 @@ def main():
     from Base import Base
 
     bird = Bird(250, 300)
+    pipe = Pipe(WIN_WIDTH+30, WIN_HEIGHT, WIN_WIDTH)
     clock = pygame.time.Clock()
 
     run = True
@@ -42,7 +58,13 @@ def main():
                     bird.jump()
 
         bird.move()
-        draw_window(WIN, bird)
+        print(len(PIPE_QUEUE))
+        if len(PIPE_QUEUE) > 0:
+            new_pipe = PIPE_QUEUE.pop()
+            new_pipe.set_reset_x(WIN_WIDTH+30)
+        pipe.move()
+
+        draw_window(WIN, bird, pipe)
     pygame.quit()
     quit()
 
